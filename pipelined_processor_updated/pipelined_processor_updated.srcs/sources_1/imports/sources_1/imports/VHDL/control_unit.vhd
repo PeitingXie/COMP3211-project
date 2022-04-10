492cf8d80c4550268d429742e7862943c95239eb
Katelyn <85672974+Katelyn19@users.noreply.github.com>
2022-04-10 23:37:38 +1000

    Added a fixed version of Katelyn's lab03 pipelined processor
    Previously some necessary VHD files were located in my local files. I have created a new project with these VHD files so that it is not located on my local disk.

---
+++ pipelined_processor_updated/pipelined_processor_updated.srcs/sources_1/imports/sources_1/imports/VHDL/control_unit.vhd
@@ -0,0 +1,104 @@
+---------------------------------------------------------------------------
+-- control_unit.vhd - Control Unit Implementation
+--
+-- Notes: refer to headers in single_cycle_core.vhd for the supported ISA.
+--
+--  control signals:
+--     reg_dst    : asserted for ADD instructions, so that the register
+--                  destination number for the 'write_register' comes from
+--                  the rd field (bits 3-0). 
+--     reg_write  : asserted for ADD and LOAD instructions, so that the
+--                  register on the 'write_register' input is written with
+--                  the value on the 'write_data' port.
+--     alu_src    : asserted for LOAD and STORE instructions, so that the
+--                  second ALU operand is the sign-extended, lower 4 bits
+--                  of the instruction.
+--     mem_write  : asserted for STORE instructions, so that the data 
+--                  memory contents designated by the address input are
+--                  replaced by the value on the 'write_data' input.
+--     mem_to_reg : asserted for LOAD instructions, so that the value fed
+--                  to the register 'write_data' input comes from the
+--                  data memory.
+--
+--
+-- Copyright (C) 2006 by Lih Wen Koh (lwkoh@cse.unsw.edu.au)
+-- All Rights Reserved. 
+--
+-- The single-cycle processor core is provided AS IS, with no warranty of 
+-- any kind, express or implied. The user of the program accepts full 
+-- responsibility for the application of the program and the use of any 
+-- results. This work may be downloaded, compiled, executed, copied, and 
+-- modified solely for nonprofit, educational, noncommercial research, and 
+-- noncommercial scholarship purposes provided that this notice in its 
+-- entirety accompanies all copies. Copies of the modified software can be 
+-- delivered to persons who use it solely for nonprofit, educational, 
+-- noncommercial research, and noncommercial scholarship purposes provided 
+-- that this notice in its entirety accompanies all copies.
+--
+---------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity control_unit is
+    port ( opcode     : in  std_logic_vector(3 downto 0);
+           reg_dst    : out std_logic;
+           reg_write  : out std_logic;
+           alu_src    : out std_logic;
+           mem_write  : out std_logic;
+           mem_to_reg : out std_logic_vector(1 downto 0);
+           read_byte  : out std_logic;
+           alu_ctr    : out std_logic_vector(1 downto 0));
+end control_unit;
+
+architecture behavioural of control_unit is
+
+constant OP_LOAD  : std_logic_vector(3 downto 0) := "0001";
+constant OP_LOADB : std_logic_vector(3 downto 0) := "0010";
+constant OP_STORE : std_logic_vector(3 downto 0) := "0011";
+constant OP_SLT   : std_logic_vector(3 downto 0) := "0100";
+constant OP_SRR   : std_logic_vector(3 downto 0) := "0101";
+constant OP_ADD   : std_logic_vector(3 downto 0) := "1000";
+
+signal sig_alu_ctr_l : std_logic;
+signal sig_alu_ctr_r : std_logic;
+
+begin
+
+    reg_dst    <= '1' when (opcode = OP_ADD
+                            or opcode = OP_SLT
+                            or opcode = OP_SRR) else
+                  '0';
+
+    reg_write  <= '1' when (opcode = OP_ADD 
+                            or opcode = OP_LOAD
+                            or opcode = OP_LOADB
+                            or opcode = OP_SLT
+                            or opcode = OP_SRR) else
+                  '0';
+    
+    alu_src    <= '1' when (opcode = OP_LOAD 
+                           or opcode = OP_STORE
+                           or opcode = OP_LOADB) else
+                  '0';
+                 
+    mem_write  <= '1' when opcode = OP_STORE else
+                  '0';
+                 
+    with opcode select
+        mem_to_reg <= "01" when OP_LOAD,
+                      "01" when OP_LOADB,
+                      "10" when OP_SLT,
+                      "11" when OP_SRR,
+                      "00" when others;
+                               
+    read_byte  <= '1' when opcode = OP_LOADB else
+                  '0';
+                  
+    with opcode select
+        alu_ctr    <= "01" when OP_LOADB,
+                      "10" when OP_SLT,
+                      "00" when others;
+
+end behavioural;
---
+++ pipelined_processor_updated/pipelined_processor_updated.srcs/sources_1/imports/sources_1/new/slt_result.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09.03.2022 13:44:36
+-- Design Name: 
+-- Module Name: slt_result - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity slt_result_calc is
+    Port ( msb : in std_logic;
+           slt_result : out STD_LOGIC_VECTOR (15 downto 0));
+end slt_result_calc;
+
+architecture Behavioral of slt_result_calc is
+
+begin
+    slt_result(15 downto 1) <= "000000000000000";
+    slt_result(0) <= msb;
+end Behavioral;
